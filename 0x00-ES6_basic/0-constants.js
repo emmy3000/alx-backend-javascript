4715348f66251e6367efe24339287c3e9707d712
Emeka Emodi <emodiemeka@gmail.com>
2023-10-02 17:23:51 -0700

    feat: Add script demonstrating the use of const and let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+/**
+ * Script demonstrates the use of `const` and `let` for variable instantiation.
+ */
+export function taskFirst() {
+  const task = "I prefer const when I can.";
+  return task;
+}
+
+export function getLast() {
+  return " is okay";
+}
+
+export function taskNext() {
+  let combination = "But sometimes let";
+  combination += getLast();
+
+  return combination;
+}
