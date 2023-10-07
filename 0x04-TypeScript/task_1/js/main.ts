abddcc9bd020724d104788658441b34332fd22c9
Emeka Emodi <emodiemeka@gmail.com>
2023-10-07 00:04:59 +0000

    feat: Implement Teacher class with defined and dynamic attributes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,19 +2,29 @@
  * Implements a "Teacher" class with defined and dynamic attributes.
  */
 class Teacher {
-  private firstName: string;
-  private lastName: string;
+  protected firstName: string;
+  protected lastName: string;
   private fullTimeEmployee: boolean = true;
   private yearsOfExperience?: number;
-  private location: string;
+  protected location: string;
   [key: string]: any;
 
+  /**
+   * Creates an instance of Teacher.
+   * @param {string} firstName - The first name of the teacher.
+   * @param {string} lastName - The last name of the teacher.
+   * @param {string} location - The location of the teacher.
+   */
   constructor(firstName: string, lastName: string, location: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.location = location;
   }
 
+  /**
+   * Gets the full name of the teacher.
+   * @returns {string} The full name.
+   */
   getFullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
@@ -26,18 +36,58 @@ teacher3.contract = false; // Adding an additional attribute
 console.log(teacher3);
 
 /**
- * Interface for Directors extending Teacher class.
+ * Class for Directors extending Teacher class.
  */
-interface Directors extends Teacher {
+class Directors extends Teacher {
   numberOfReports: number;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    location: string,
+    numberOfReports: number
+  ) {
+    super(firstName, lastName, location);
+    this.numberOfReports = numberOfReports;
+  }
+
+  getFullName(): string {
+    return super.getFullName();
+  }
+
+  // Custom method to print properties
+  printProperties(): void {
+    for (const prop in this) {
+      if (this.hasOwnProperty(prop)) {
+        console.log(`${prop}: ${this[prop]}`);
+      }
+    }
+  }
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+const director1 = new Directors('John', 'Doe', 'London', 17);
+director1.printProperties();
 
-console.log(director1);
+/**
+ * Function to print the first letter of the firstName and the full lastName.
+ * @param {string} firstName - The first name of the teacher.
+ * @param {string} lastName - The last name of the teacher.
+ * @returns {string} A string in the format "X. LastName".
+ */
+function printTeacher(firstName: string, lastName: string): string {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstInitial}. ${lastName}`;
+  return fullName;
+}
+
+/**
+ * Interface for the printTeacher function.
+ */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Example usage:
+const teacherName: printTeacherFunction = printTeacher;
+const result = teacherName('John', 'Doe');
+console.log(result); // Output: "J. Doe"
