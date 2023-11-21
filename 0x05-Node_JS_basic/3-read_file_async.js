f3749eec5229101025ee5007fdbf9ad60e98468f
Emeka Emodi <emodiemeka@gmail.com>
2023-11-21 09:24:24 +0000

    feat: Implement 'countStudents' function to read CSV, count students per field, and display results asynchronously using Node.js fs module and Promises.

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,49 +1,45 @@
 // 3-read_file_async.js
 
-const { readFile } = require('fs');
+const { readFile } = require('fs').promises;
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf-8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
+  return readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.split('\n').filter(line => line.trim() !== '');
 
-        const students = {};
-        const fields = {};
-        let totalStudents = 0;
+      const students = {};
+      const fields = {};
+      let totalStudents = 0;
 
-        lines.forEach((line, index) => {
-          // Skip header line
-          if (index !== 0) {
-            const [firstName, lastName, age, field] = line.split(',');
-            if (!fields[field]) {
-              fields[field] = 1;
-            } else {
-              fields[field] += 1;
-            }
-            if (!students[field]) {
-              students[field] = [firstName];
-            } else {
-              students[field].push(firstName);
-            }
-            totalStudents += 1;
+      lines.forEach((line, index) => {
+        // Skip header line
+        if (index !== 0) {
+          const [firstName, lastName, age, field] = line.split(',');
+          if (!fields[field]) {
+            fields[field] = 1;
+          } else {
+            fields[field] += 1;
           }
-        });
+          if (!students[field]) {
+            students[field] = [firstName];
+          } else {
+            students[field].push(firstName);
+          }
+          totalStudents += 1;
+        }
+      });
 
-        console.log(`Number of students: ${totalStudents}`);
+      console.log(`Number of students: ${totalStudents}`);
 
-        Object.keys(fields).forEach(field => {
-          const numStudentsInField = fields[field];
-          const studentList = students[field].join(', ');
-          console.log(`Number of students in ${field}: ${numStudentsInField}. List: ${studentList}`);
-        });
-
-        resolve();
-      }
+      Object.keys(fields).forEach(field => {
+        const numStudentsInField = fields[field];
+        const studentList = students[field].join(', ');
+        console.log(`Number of students in ${field}: ${numStudentsInField}. List: ${studentList}`);
+      });
+    })
+    .catch((error) => {
+      console.error('Cannot load the database:', error.message);
     });
-  });
 }
 
 module.exports = countStudents;
