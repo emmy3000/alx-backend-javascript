f010ea76f409857a65516cb35ef376556e4c6ca5
Emeka Emodi <emodiemeka@gmail.com>
2023-11-22 03:24:24 +0000

    feat: Implement Express application to handle requests and respond with a plain text message

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,21 @@
-// 6-http_express.js
+/**
+ * Create an Express application, listen on a specified port,
+ * and respond with a plain text message.
+ *
+ * @module http_express
+ * @returns {Object} Express application instance.
+ */
 
 const express = require('express');
+
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port=1245, () => {
+app.listen(port, () => {
 });
 
 module.exports = app;
