b83bfd102648a34a73e04f34e5e13b2df6d0daff
Emeka Emodi <emodiemeka@gmail.com>
2023-11-22 03:10:52 +0000

    feat: Implement user input reading from stdin and display the name

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,15 @@
-// 1-stdin.js
+/**
+ * Read user input from the standard input (stdin) and display the name.
+ *
+ * @param {string} name - The name entered by the user.
+ * @returns {void}
+ */
 
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
