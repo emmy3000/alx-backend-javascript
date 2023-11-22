03f0671cb6a5dce7505cdadff3e3129031d7ab3a
Emeka Emodi <emodiemeka@gmail.com>
2023-11-22 03:06:04 +0000

    feat: Add function to display a message to the console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,9 @@
-// 0-console.js
+/**
+ * Display a message to the console.
+ *
+ * @param {string} message - The message to be displayed.
+ * @returns {void}
+ */
 
 function displayMessage(message) {
   console.log(message);
