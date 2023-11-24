ed6141cb962a696e7d6a8d07eef2daf796a28d86
Emeka Emodi <emodiemeka@gmail.com>
2023-11-24 21:27:02 +0000

    Implement comprehensive test suite for 'calculateNumber' function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,52 +9,23 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
+  function testRoundingBehavior(description, a, b, expected) {
+    it(description, () => {
+      assert.strictEqual(calculateNumber(a, b), expected);
+    });
+  }
+
   // Basic test cases
-  it('should round and return the sum for integer values', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should round and return the sum for decimal values with one decimal place', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should round and return the sum for decimal values with two decimal places', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should round and return the sum for decimal values with two decimal places', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should round down both a and b when they have fractional parts', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
-  });
-
-  it('should round down b when it has a fractional part', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
-  });
-
-  it('should round down a and b when they both have fractional parts', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
-  });
-
-  it('should round down a when it has a fractional part', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
-  });
-
-  it('should round up b when it has a fractional part', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
-  });
-
-  it('should round up both a and b when they have fractional parts', () => {
-    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
-  });
-
-  it('should round up a when it has a fractional part', () => {
-    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
-  });
-
-  it('should round down both a and b when they have fractional parts with trailing 9\'s', () => {
-    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
-  });
+  testRoundingBehavior('should round and return the sum for integer values', 1, 3, 4);
+  testRoundingBehavior('should round and return the sum for decimal values with one decimal place', 1, 3.7, 5);
+  testRoundingBehavior('should round and return the sum for decimal values with two decimal places', 1.2, 3.7, 5);
+  testRoundingBehavior('should round and return the sum for decimal values with two decimal places', 1.5, 3.7, 6);
+  testRoundingBehavior('should round down both a and b when they have fractional parts', 1.0, 2.0, 3);
+  testRoundingBehavior('should round down b when it has a fractional part', 1.0, 2.4, 3);
+  testRoundingBehavior('should round down a and b when they both have fractional parts', 1.4, 2.4, 3);
+  testRoundingBehavior('should round down a when it has a fractional part', 1.4, 2.0, 3);
+  testRoundingBehavior('should round up b when it has a fractional part', 1.0, 2.5, 4);
+  testRoundingBehavior('should round up both a and b when they have fractional parts', 2.6, 2.5, 6);
+  testRoundingBehavior('should round up a when it has a fractional part', 2.6, 2.0, 5);
+  testRoundingBehavior('should round down both a and b when they have fractional parts with trailing 9\'s', 2.499999, 3.499999, 5);
 });
