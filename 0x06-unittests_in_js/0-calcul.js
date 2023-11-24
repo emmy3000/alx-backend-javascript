3fb7f21409bcf31c44fd1a34fee75efa8ce59ef9
Emeka Emodi <emodiemeka@gmail.com>
2023-11-24 22:03:19 +0000

    Implement 'calculateNumber ' function for rounding and summing

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,9 @@
+/*
+  calculateNumber.js
+
+  This module defines the 'calculateNumber' function, which takes two numerical arguments,
+  rounds them, and returns the sum of their rounded values.
+*/
 function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
