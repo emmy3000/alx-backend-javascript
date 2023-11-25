173600c1fe9bce0dce4da0bd6572cb1855c64a7e
Emeka Emodi <emodiemeka@gmail.com>
2023-11-25 00:36:00 +0000

    feat: create Utils module with 'calculateNumber' function

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,38 @@
+/*
+  utils.js
+
+  This module provides a set of utility functions, including calculateNumber.
+  The calculateNumber function takes three arguments:
+  - type: A string specifying the operation type ('SUM', 'SUBTRACT', or 'DIVIDE').
+  - a: The first number for the calculation.
+  - b: The second number for the calculation.
+
+  The function performs different operations based on the provided type:
+  - SUM: Adds the rounded values of a and b.
+  - SUBTRACT: Subtracts the rounded value of b from the rounded value of a.
+  - DIVIDE: Divides the rounded value of a by the rounded value of b.
+            Returns 'Error' if b is rounded to 0.
+*/
+
+const Utils = {
+  calculateNumber: (type, a, b) => {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+  
+    switch (type) {
+      case 'SUM':
+        return roundedA + roundedB;
+      case 'SUBTRACT':
+        return roundedA - roundedB;
+      case 'DIVIDE':
+        if (roundedB === 0) {
+          return 'Error';
+        }
+        return roundedA / roundedB;
+      default:
+        throw new Error('Invalid type. Supported types are SUM, SUBTRACT, and DIVIDE.');
+    }
+  },
+};
+
+module.exports = Utils;
